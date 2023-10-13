e1ce2cc9f8e825ab7465f866305f6fb17f2ca295
Rashad Hamid <mr_rashad@email.com>
2023-10-13 17:12:28 +0400

    add

--- src/main/java/com/company/entity/User.java
+++ src/main/java/com/company/entity/User.java
@@ -1,9 +1,20 @@
 package com.company.entity;
 
+import com.company.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 public class User {
 
     public static void main(String[] args) {
-
+        Student student=new Student();
+        student.setName("Farhad");
+        student.setSurname("Valizade");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(student);
+        transaction.commit();
+        session.close();
     }
 
 }
