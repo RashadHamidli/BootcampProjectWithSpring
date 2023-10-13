e983c9a47055f09440dcb9aa7d4a7fa51486dd12
Rashad Hamid <mr_rashad@email.com>
2023-10-13 14:54:20 +0400

    add

---
+++ src/main/java/com/company/Student.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public record Student(long id,
+                      String name,
+                      String surname) {
+
+}
---
+++ src/main/java/com/company/Teacher.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Teacher {
+    private int id;
+    private String name;
+    private String surname;
+
+    public Teacher(int id, String name, String surname) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+    }
+}
