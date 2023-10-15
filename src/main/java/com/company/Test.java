33ef450ceda2e4e4a0f89bedf8788af40b95d433
Rashad Hamid <mr_rashad@email.com>
2023-10-15 22:19:02 +0400

    add

---
+++ src/main/java/com/company/Test.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int i='a';
+        System.out.println(i);
+        int[] arr= {10};
+    }
+}
