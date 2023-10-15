ba633c643757e6b7884ad542d8f310adfe62c1df
Rashad Hamid <mr_rashad@email.com>
2023-10-15 19:32:11 +0400

    add

--- src/main/java/com/company/demoProj/PostgresSelectTable.java
+++ src/main/java/com/company/demoProj/PostgresSelectTable.java
@@ -1,4 +1,26 @@
 package com.company.demoProj;
 
+import com.company.entity.Student;
+
+import java.sql.*;
+
 public class PostgresSelectTable {
+    public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/user_register", "postgres", "admin");
+             Statement statement = connection.createStatement()) {
+            ResultSet resultSet = statement.executeQuery("select * from student");
+            while (resultSet.next()) {
+                String name = resultSet.getString("student_name");
+                String surname = resultSet.getString("student_surname");
+                String email = resultSet.getString("student_email");
+
+                String format = String.format("%s, %s, %s", name, surname, email);
+                System.out.println(format);
+            }
+        } catch (SQLException e) {
+            e.getStackTrace();
+            System.out.println("failed");
+        }
+
+    }
 }
---
+++ src/main/java/com/company/entity/Student.java
@@ -0,0 +1,32 @@
+package com.company.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "student")
+@RequiredArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "student_id")
+    private int id;
+    @Column(name = "student_name")
+    private String name;
+    @Column(name = "student_surname")
+    private String surname;
+    @Column(name = "student_email")
+    private String email;
+
+    public Student(String name, String surname, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
+}
