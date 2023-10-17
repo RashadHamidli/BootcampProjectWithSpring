5e907722205929f74519d14f5ef4fd91aeba8d68
Rashad Hamid <mr_rashad@email.com>
2023-10-17 15:06:14 +0400

    add

--- src/main/java/com/company/jpa/jpa/Main.java
+++ src/main/java/com/company/jpa/jpa/Main.java
@@ -1,7 +1,21 @@
 package com.company.jpa.jpa;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("JDK21");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("rashadPU");
+        EntityManager em = emf.createEntityManager();
+        Query nativeQuery = em.createNativeQuery("select * from student s ");
+        System.out.println("-----------------------------");
+        for (Object o : nativeQuery.getResultList()) {
+            System.out.println(o);
+        }
+        System.out.println("-----------------------------");
+        em.close();
+        emf.close();
     }
 }
--- src/main/java/com/company/jpa/jpa/Test.java
+++ src/main/java/com/company/jpa/jpa/Test.java
@@ -1,4 +1,17 @@
 package com.company.jpa.jpa;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+
 public class Test {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("rashadPU");
+        EntityManager em = emf.createEntityManager();
+        Query nativeQuery = em.createNativeQuery("select * from student where student_id=1;");
+        emf.close();
+        em.close();
+
+    }
 }
