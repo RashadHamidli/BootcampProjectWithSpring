bcc4555ff1f2177c7183bd7168980a3d526826ba
Rashad Hamid <mr_rashad@email.com>
2023-10-20 00:15:39 +0400

    add

--- src/main/java/com/company/spring/example/AMessage.java
+++ src/main/java/com/company/spring/example/AMessage.java
@@ -1,5 +1,8 @@
 package com.company.spring.example;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class AMessage implements IMessage{
     @Override
     public void print() {
