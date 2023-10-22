93261cc1d44ccddc16fe5e092731ec7c56bca3e3
Rashad Hamid <mr_rashad@email.com>
2023-10-22 20:18:49 +0400

    add

--- src/main/java/com/company/spring/example/Main.java
+++ src/main/java/com/company/spring/example/Main.java
@@ -7,11 +7,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        IMessage aMessage = context.getBean("messageA", IMessage.class);
-        aMessage.print();
-        IMessage bMessage = context.getBean("messageB", IMessage.class);
-        bMessage.print();
+        ApplicationContext context = new AnnotationConfigApplicationContext("rashadPU");
+        System.out.println("aaaaaa");
+//        IMessage aMessage = context.getBean("messageA", IMessage.class);
+//        aMessage.print();
+//        IMessage bMessage = context.getBean("messageB", IMessage.class);
+//        bMessage.print();
 
     }
 }
