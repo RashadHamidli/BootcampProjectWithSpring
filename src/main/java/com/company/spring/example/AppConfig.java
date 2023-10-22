0498fd758892d54f33f06572029f4d83e2417467
Rashad Hamid <mr_rashad@email.com>
2023-10-22 20:27:58 +0400

    add

--- src/main/java/com/company/spring/example/AppConfig.java
+++ src/main/java/com/company/spring/example/AppConfig.java
@@ -1,59 +1,20 @@
-//package com.company.spring.example;
-//
-//import jakarta.persistence.EntityManagerFactory;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.ComponentScan;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-//import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-//import org.springframework.orm.jpa.JpaTransactionManager;
-//import org.springframework.orm.jpa.JpaVendorAdapter;
-//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-//import org.springframework.orm.jpa.vendor.Database;
-//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-//import org.springframework.transaction.PlatformTransactionManager;
-//
-//import javax.sql.DataSource;
-//
-//@Configuration
-//@ComponentScan
-//@EnableJpaRepositories
-//public class AppConfig {
-//
+package com.company.spring.example;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.company.spring.example")
+public class AppConfig {
 //    @Bean
-//    public DataSource dataSource() {
-//        return new EmbeddedDatabaseBuilder()
-//                .setType(EmbeddedDatabaseType.H2)
-//                .build();
+//    public IMessage messageA() {
+//        return new AMessage();
 //    }
-//
 //    @Bean
-//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
-//            DataSource dataSource,
-//            JpaVendorAdapter jpaVendorAdapter
-//    ) {
-//        LocalContainerEntityManagerFactoryBean lcemf;
-//        lcemf = new LocalContainerEntityManagerFactoryBean();
-//        lcemf.setDataSource(dataSource);
-//        lcemf.setJpaVendorAdapter(jpaVendorAdapter);
-//        lcemf.setPackagesToScan("com.yusufsezer");
-//        return lcemf;
+//    public IMessage messageB() {
+//        return new BMessage();
 //    }
-//
-//    @Bean
-//    public JpaVendorAdapter jpaVendorAdapter() {
-//        HibernateJpaVendorAdapter hibernateJpaVendorAdapter;
-//        hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-//        hibernateJpaVendorAdapter.setShowSql(true);
-//        hibernateJpaVendorAdapter.setGenerateDdl(true);
-//        hibernateJpaVendorAdapter.setDatabase(Database.POSTGRESQL);
-//        return hibernateJpaVendorAdapter;
-//    }
-//
-//    @Bean
-//    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
-//        return new JpaTransactionManager(emf);
-//    }
-//
-//}
+    // Bu sinifdə Spring komponentlərinin konfiqurasiyası təyin edilir
+    // @ComponentScan ildə müəyyənləşdirilmiş bazaya uyğun komponentləri axtarır
+}
