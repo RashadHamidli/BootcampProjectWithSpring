cb5a6e67a197a88f0c220124f9f43e529f7b8b75
Rashad Hamid <mr_rashad@email.com>
2023-10-22 18:51:13 +0400

    add

--- src/main/java/com/company/spring/example/Main.java
+++ src/main/java/com/company/spring/example/Main.java
@@ -7,9 +7,11 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         IMessage aMessage = context.getBean("messageA", IMessage.class);
         aMessage.print();
+        IMessage bMessage = context.getBean("messageB", IMessage.class);
+        bMessage.print();
 
     }
 }
